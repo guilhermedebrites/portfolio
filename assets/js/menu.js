a64a0e130b17f84b9df4e9a2e507706d0f1a4239
Guilherme DeBrites <100132323+guilhermedebrites@users.noreply.github.com>
2022-08-03 21:46:49 -0300

    02 - Animações para melhor experiência do usuário.
    Atualizações:
    
    - Retirada da logo do header ao abrir o menu hamburguer.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,7 +3,9 @@ const menu = document.querySelector('.menu-bar');
  
 function toogleMenu() {
     const list = document.querySelector('.menu-list');
+    const logo = document.querySelector('.logo-header');
     list.classList.toggle('active');
+    logo.classList.toggle('logo-disable');
 }
 
 
