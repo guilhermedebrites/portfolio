a4fedbd3262c230629dfa2e4c6686dbbfe8de741
Guilherme DeBrites <100132323+guilhermedebrites@users.noreply.github.com>
2022-08-20 17:07:21 -0300

    03 - Fix
    Fix:
    Responsividade
    Menu position
    
    Add:
    New project

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,11 +3,13 @@ const projectAluraBooks = document.getElementById('alurabooks');
 const projectHzc = document.getElementById('hzc');
 const projectOpttech = document.getElementById('opttech');
 const projectAluraPlus = document.getElementById('aluraplus');
+const projectRobotron= document.getElementById('robotron2000');
 
 projectAluraBooks.addEventListener('click', () => inicialModal('modal-alurabooks'));
 projectHzc.addEventListener('click', () => inicialModal('modal-hzc'));
 projectOpttech.addEventListener('click', () => inicialModal('modal-opttech'));
 projectAluraPlus.addEventListener('click', () => inicialModal('modal-aluraplus'));
+projectRobotron.addEventListener('click', () => inicialModal('modal-robotron'));
 
 function inicialModal(modalId){
     const modal = document.getElementById(modalId);
